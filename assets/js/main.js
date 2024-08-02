c39638b5b24635800eff8f082fbd5307d0695e94
morenoal <skinymoreno98@gmail.com>
2024-08-01 23:51:32 -0700

    switched to SaaS template

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,173 +1,357 @@
-/*
-	Spectral by HTML5 UP
-	html5up.net | @ajlkn
-	Free for personal and commercial use under the CCA 3.0 license (html5up.net/license)
-*/
+(function () {
+  "use strict";
 
-(function($) {
+  // ======= Sticky
+  window.onscroll = function () {
+    const header_navbar = document.querySelector(".navbar-area");
+    const sticky = header_navbar.offsetTop;
+    const logo = document.querySelector(".navbar-brand img");
 
-	var	$window = $(window),
-		$body = $('body'),
-		$wrapper = $('#page-wrapper'),
-		$banner = $('#banner'),
-		$header = $('#header');
+    if (window.pageYOffset > sticky) {
+      header_navbar.classList.add("sticky");
+      logo.src = "assets/images/logo/logo-2.svg";
+    } else {
+      header_navbar.classList.remove("sticky");
+      logo.src = "assets/images/logo/logo.svg";
+    }
 
-	// Breakpoints.
-		breakpoints({
-			xlarge:   [ '1281px',  '1680px' ],
-			large:    [ '981px',   '1280px' ],
-			medium:   [ '737px',   '980px'  ],
-			small:    [ '481px',   '736px'  ],
-			xsmall:   [ null,      '480px'  ]
-		});
+    // show or hide the back-top-top button
+    const backToTop = document.querySelector(".back-to-top");
+    if (
+      document.body.scrollTop > 50 ||
+      document.documentElement.scrollTop > 50
+    ) {
+      backToTop.style.display = "flex";
+    } else {
+      backToTop.style.display = "none";
+    }
+  };
 
-	// Play initial animations on page load.
-		$window.on('load', function() {
-			window.setTimeout(function() {
-				$body.removeClass('is-preload');
-			}, 100);
-		});
+  // ==== for menu scroll
+  const pageLink = document.querySelectorAll(".page-scroll");
 
-	// Mobile?
-		if (browser.mobile)
-			$body.addClass('is-mobile');
-		else {
+  pageLink.forEach((elem) => {
+    elem.addEventListener("click", (e) => {
+      e.preventDefault();
+      document.querySelector(elem.getAttribute("href")).scrollIntoView({
+        behavior: "smooth",
+        offsetTop: 1 - 60,
+      });
+    });
+  });
 
-			breakpoints.on('>medium', function() {
-				$body.removeClass('is-mobile');
-			});
+  // section menu active
+  function onScroll(event) {
+    const sections = document.querySelectorAll(".page-scroll");
+    const scrollPos =
+      window.pageYOffset ||
+      document.documentElement.scrollTop ||
+      document.body.scrollTop;
 
-			breakpoints.on('<=medium', function() {
-				$body.addClass('is-mobile');
-			});
+    for (let i = 0; i < sections.length; i++) {
+      const currLink = sections[i];
+      const val = currLink.getAttribute("href");
+      const refElement = document.querySelector(val);
+      const scrollTopMinus = scrollPos + 73;
+      if (
+        refElement.offsetTop <= scrollTopMinus &&
+        refElement.offsetTop + refElement.offsetHeight > scrollTopMinus
+      ) {
+        document.querySelector(".page-scroll").classList.remove("active");
+        currLink.classList.add("active");
+      } else {
+        currLink.classList.remove("active");
+      }
+    }
+  }
 
-		}
+  window.document.addEventListener("scroll", onScroll);
 
-	// Scrolly.
-		$('.scrolly')
-			.scrolly({
-				speed: 1500,
-				offset: $header.outerHeight()
-			});
+  //===== close navbar-collapse when a  clicked
+  let navbarToggler = document.querySelector(".navbar-toggler");
+  const navbarCollapse = document.querySelector(".navbar-collapse");
 
-	// Menu.
-		$('#menu')
-			.append('<a href="#menu" class="close"></a>')
-			.appendTo($body)
-			.panel({
-				delay: 500,
-				hideOnClick: true,
-				hideOnSwipe: true,
-				resetScroll: true,
-				resetForms: true,
-				side: 'right',
-				target: $body,
-				visibleClass: 'is-menu-visible'
-			});
+  document.querySelectorAll(".page-scroll").forEach((e) =>
+    e.addEventListener("click", () => {
+      navbarToggler.classList.remove("active");
+      navbarCollapse.classList.remove("show");
+    })
+  );
+  navbarToggler.addEventListener("click", function () {
+    navbarToggler.classList.toggle("active");
+    navbarCollapse.classList.toggle("show");
+  });
 
-	// Header.
-		if ($banner.length > 0
-		&&	$header.hasClass('alt')) {
+  // ========= glightbox
+  const myGallery = GLightbox({
+    href: "https://www.youtube.com/watch?v=r44RKWyfcFw",
+    type: "video",
+    source: "youtube", //vimeo, youtube or local
+    width: 900,
+    autoplayVideos: true,
+  });
 
-			$window.on('resize', function() { $window.trigger('scroll'); });
+  //====== counter up
+  const cu = new counterUp({
+    start: 0,
+    duration: 2000,
+    intvalues: true,
+    interval: 100,
+    append: "k",
+  });
+  cu.start();
 
-			$banner.scrollex({
-				bottom:		$header.outerHeight() + 1,
-				terminate:	function() { $header.removeClass('alt'); },
-				enter:		function() { $header.addClass('alt'); },
-				leave:		function() { $header.removeClass('alt'); }
-			});
+  //======== tiny slider for clients
+  tns({
+    container: ".testimonial-active",
+    autoplay: true,
+    autoplayTimeout: 5000,
+    autoplayButtonOutput: false,
+    mouseDrag: true,
+    gutter: 24,
+    nav: true,
+    navPosition: "bottom",
+    controls: false,
+    controlsText: [
+      '<i class="lni lni-chevron-left"></i>',
+      '<i class="lni lni-chevron-right"></i>',
+    ],
+    items: 1,
+    responsive: {
+      768: {
+        items: 2,
+      },
+      992: {
+        items: 3,
+      },
+    },
+  });
 
-		}
-		setTimeout(()=>{
-			$('#banner .more.scrolly').removeClass('initial-load');
-		}, 4000)
-		
+  //=====  WOW active
+  new WOW().init();
 
-	// Contact Form.
-		$('#contactForm')
-			.append('<a href="#contactForm" class="close"></a>')
-			.appendTo($body)
-			.panel({
-				delay: 500,
-				hideOnClick: true,
-				hideOnSwipe: true,
-				resetScroll: true,
-				resetForms: true,
-				target: $body,
-				visibleClass: 'is-contactForm-visible'
-			});
+  //=====  particles
+  if (document.getElementById("particles-1"))
+    particlesJS("particles-1", {
+      particles: {
+        number: {
+          value: 40,
+          density: {
+            enable: !0,
+            value_area: 4000,
+          },
+        },
+        color: {
+          value: ["#FFFFFF", "#FFFFFF", "#FFFFFF"],
+        },
+        shape: {
+          type: "circle",
+          stroke: {
+            width: 0,
+            color: "#fff",
+          },
+          polygon: {
+            nb_sides: 5,
+          },
+          image: {
+            src: "img/github.svg",
+            width: 33,
+            height: 33,
+          },
+        },
+        opacity: {
+          value: 0.15,
+          random: !0,
+          anim: {
+            enable: !0,
+            speed: 0.2,
+            opacity_min: 0.15,
+            sync: !1,
+          },
+        },
+        size: {
+          value: 50,
+          random: !0,
+          anim: {
+            enable: !0,
+            speed: 2,
+            size_min: 5,
+            sync: !1,
+          },
+        },
+        line_linked: {
+          enable: !1,
+          distance: 150,
+          color: "#ffffff",
+          opacity: 0.4,
+          width: 1,
+        },
+        move: {
+          enable: !0,
+          speed: 1,
+          direction: "top",
+          random: !0,
+          straight: !1,
+          out_mode: "out",
+          bounce: !1,
+          attract: {
+            enable: !1,
+            rotateX: 600,
+            rotateY: 600,
+          },
+        },
+      },
+      interactivity: {
+        detect_on: "canvas",
+        events: {
+          onhover: {
+            enable: !1,
+            mode: "bubble",
+          },
+          onclick: {
+            enable: !1,
+            mode: "repulse",
+          },
+          resize: !0,
+        },
+        modes: {
+          grab: {
+            distance: 400,
+            line_linked: {
+              opacity: 1,
+            },
+          },
+          bubble: {
+            distance: 250,
+            size: 0,
+            duration: 2,
+            opacity: 0,
+            speed: 3,
+          },
+          repulse: {
+            distance: 400,
+            duration: 0.4,
+          },
+          push: {
+            particles_nb: 4,
+          },
+          remove: {
+            particles_nb: 2,
+          },
+        },
+      },
+      retina_detect: !0,
+    });
 
-		$('#form_contact').submit(function(event) {
-			event.preventDefault(); // Prevent default form submission
-			// Your form handling logic here
-			submitForm(event)
-		});
-		var contactLinks = document.querySelectorAll('a[href="#contactForm"]');
-
-	// Iterate through each anchor element and add event listener
-		contactLinks.forEach(function(link) {
-			link.addEventListener('click', function(event) {
-				window.scrollTo({
-					top: document.getElementById('contactForm').getBoundingClientRect().top + window.y,
-					behavior: 'smooth'
-				});
-			});
-		});
-
-})(jQuery);
-
-function submitForm(e){
-	Email.send({
-		SecureToken : "8dbf79ba-a662-470d-97ee-0a10f0eaddef",
-		To : document.getElementById('form_contact-email').value,
-		From : "skinymoreno98@gmail.com",
-		// FromName: "support@varwebsolutions.com",
-		Subject : "Email Confirmation Notice",
-		Body : `
-			Hi ${document.getElementById('form_contact-name').value},</br>
-			</br>
-			Thank you for reaching out to our team at Varweb Solutions! This is just an email confirming we 
-			have received your message and will get back to you in 1-3 days. We appreciate your patience and look
-			forward to working with you.</br>
-			</br>
-			Best,</br>
-			Alder Moreno</br>
-			Developer @ Varweb Solutions</br>
-			(916) 390-1005</br>
-			</br>
-			</br>
-			<hr>
-			</br>
-			${document.getElementById('form_contact-body').value}
-			`
-	}).then((message) => {
-		console.log(message);
-	});
-	Email.send({
-		SecureToken : "8dbf79ba-a662-470d-97ee-0a10f0eaddef",
-		To : 'skinymoreno98@gmail.com',
-		From : "skinymoreno98@gmail.com",
-		// FromName: document.getElementById('form_contact-email').value,
-		Subject : document.getElementById('form_contact-subject').value,
-		Body : `
-			${document.getElementById('form_contact-body').value}</br>
-			</br>
-			Email from </br>
-			${document.getElementById('form_contact-name').value}</br>
-		`
-	}).then((message) => {
-		if(message == 'OK'){
-			document.getElementById('submit-feedback').innerHTML = 'Your message has been received!';
-			document.getElementById('submit-feedback').className = 'success'
-		}else{
-			document.getElementById('submit-feedback').innerHTML = 'Error sending message... <a href="mailto:morenoalder98@gmail.com">Email Us</a> instead.';
-			document.getElementById('submit-feedback').className = 'error'
-		}
-	});
-}
-
-function resetForm(){
-	document.getElementById('submit-feedback').innerHTML = '';
-	document.getElementById('submit-feedback').className = ''
-}
+  if (document.getElementById("particles-2"))
+    particlesJS("particles-2", {
+      particles: {
+        number: {
+          value: 40,
+          density: {
+            enable: !0,
+            value_area: 4000,
+          },
+        },
+        color: {
+          value: ["#FFFFFF", "#FFFFFF", "#FFFFFF"],
+        },
+        shape: {
+          type: "circle",
+          stroke: {
+            width: 0,
+            color: "#fff",
+          },
+          polygon: {
+            nb_sides: 5,
+          },
+          image: {
+            src: "img/github.svg",
+            width: 33,
+            height: 33,
+          },
+        },
+        opacity: {
+          value: 0.15,
+          random: !0,
+          anim: {
+            enable: !0,
+            speed: 0.2,
+            opacity_min: 0.15,
+            sync: !1,
+          },
+        },
+        size: {
+          value: 50,
+          random: !0,
+          anim: {
+            enable: !0,
+            speed: 2,
+            size_min: 5,
+            sync: !1,
+          },
+        },
+        line_linked: {
+          enable: !1,
+          distance: 150,
+          color: "#ffffff",
+          opacity: 0.4,
+          width: 1,
+        },
+        move: {
+          enable: !0,
+          speed: 1,
+          direction: "top",
+          random: !0,
+          straight: !1,
+          out_mode: "out",
+          bounce: !1,
+          attract: {
+            enable: !1,
+            rotateX: 600,
+            rotateY: 600,
+          },
+        },
+      },
+      interactivity: {
+        detect_on: "canvas",
+        events: {
+          onhover: {
+            enable: !1,
+            mode: "bubble",
+          },
+          onclick: {
+            enable: !1,
+            mode: "repulse",
+          },
+          resize: !0,
+        },
+        modes: {
+          grab: {
+            distance: 400,
+            line_linked: {
+              opacity: 1,
+            },
+          },
+          bubble: {
+            distance: 250,
+            size: 0,
+            duration: 2,
+            opacity: 0,
+            speed: 3,
+          },
+          repulse: {
+            distance: 400,
+            duration: 0.4,
+          },
+          push: {
+            particles_nb: 4,
+          },
+          remove: {
+            particles_nb: 2,
+          },
+        },
+      },
+      retina_detect: !0,
+    });
+})();
