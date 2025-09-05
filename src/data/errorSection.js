59a25300186234d746a989f10a67bff60da957e3
morenoal <skinymoreno98@gmail.com>
2025-09-05 11:53:10 -0700

    reverted to previous commit

--- src/data/errorSection.js
+++
@@ -1,7 +0,0 @@
-import image from "@/images/icons/404-image.png";
-
-export const errorSection = {
-  image,
-  title: "Sorry We Can't Find That Page!",
-  text: "The page you are looking for was moved, removed, renamed or never existed.",
-};
